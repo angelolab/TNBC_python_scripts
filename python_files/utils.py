54496e9edaaae4d1097880c94f0852b658c19f7e
ngreenwald <noahfgreenwald@gmail.com>
2022-12-28 15:19:30 -0800

    functional marker subsetting

--- python_files/utils.py
+++ python_files/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 from ark.utils.misc_utils import verify_in_list
@@ -48,7 +49,7 @@ def create_long_df_by_cluster(cell_table, cluster_col_name, result_name, subset_
 
     # if a subset column is specified, create df stratified by subset
     if subset_col is not None:
-        verify_in_list(cell_type_col=subset_col, cell_table_columns=cell_table.columns)
+        verify_in_list(subset_col=subset_col, cell_table_columns=cell_table.columns)
 
         # group each fov by fov and cluster
         grouped = cell_table.groupby(['fov', subset_col])
@@ -70,24 +71,21 @@ def create_long_df_by_cluster(cell_table, cluster_col_name, result_name, subset_
         counts = counts.rename(columns={cluster_col_name: 'cell_type', subset_col: 'subset',
                                         0: 'value'})
 
-        # move subset column to back to enable combining with long_df_all
-        counts = counts[['fov', 'cell_type', 'value', 'metric', 'subset']]
-
         # combine the two dataframes
         long_df_all = pd.concat([long_df_all, counts], axis=0, ignore_index=True)
 
     return long_df_all
 
 
-def create_long_df_by_functional(func_table, cluster_col_name, drop_cols, transform_func, result_name):
+def functional_df_helper(func_table, cluster_col_name, drop_cols, result_name, normalize=False):
     """Function to summarize functional marker data by cell type
 
     Args:
         func_table (pd.DataFrame): cell table containing functional markers
         cluster_col_name (str): name of the column in func_table that contains the cluster information
         drop_cols (list): list of columns to drop from func_table
-        transform_func (function): function to apply to each column in func_table
         result_name (str): name of the statistic in the summarized information df
+        normalize (bool): whether to report the total or normalized counts in the result
 
     Returns:
         pd.DataFrame: long format dataframe containing the summarized data"""
@@ -100,7 +98,10 @@ def create_long_df_by_functional(func_table, cluster_col_name, drop_cols, transf
 
     # group by specified columns
     grouped_table = func_table_small.groupby(['fov', cluster_col_name])
-    transformed = grouped_table.agg(transform_func)
+    if normalize:
+        transformed = grouped_table.agg(np.mean)
+    else:
+        transformed = grouped_table.agg(np.sum)
     transformed.reset_index(inplace=True)
 
     # reshape to long df
@@ -109,3 +110,57 @@ def create_long_df_by_functional(func_table, cluster_col_name, drop_cols, transf
     long_df = long_df.rename(columns={cluster_col_name: 'cell_type'})
 
     return long_df
+
+
+def create_long_df_by_functional(func_table, cluster_col_name, drop_cols, result_name,
+                                        subset_col=None, normalize=False):
+    """Summarize functional marker positivity by cell type, with the option to subset by an additional feature
+
+    Args:
+        func_table (pd.DataFrame): the dataframe containing information on each cell
+        cluster_col_name (str): the column name in cell_table that contains the cluster information
+        drop_cols (list): list of columns to drop from cell_table
+        result_name (str): the name of this statistic in the returned df
+        subset_col (str): the column name in cell_table to subset by
+        normalize (bool): whether to report the total or normalized counts in the result
+
+    Returns:
+        pd.DataFrame: long format dataframe containing the summarized data"""
+
+    # first generate df without subsetting
+    drop_cols_all = drop_cols.copy()
+    if subset_col is not None:
+        drop_cols_all = drop_cols + [subset_col]
+
+    long_df_all = functional_df_helper(func_table, cluster_col_name, drop_cols_all, result_name,
+                                       normalize)
+    long_df_all['subset'] = 'all'
+
+    # if a subset column is specified, create df stratified by subset
+    if subset_col is not None:
+        verify_in_list(subset_col=subset_col, cell_table_columns=func_table.columns)
+
+        # drop columns from table
+        func_table_small = func_table.loc[:, ~func_table.columns.isin(drop_cols)]
+
+        # group by specified columns
+        grouped_table = func_table_small.groupby(['fov', subset_col, cluster_col_name])
+        if normalize:
+            transformed = grouped_table.agg(np.mean)
+        else:
+            transformed = grouped_table.agg(np.sum)
+        transformed.reset_index(inplace=True)
+
+        # reshape to long df
+        long_df = pd.melt(transformed, id_vars=['fov', subset_col, cluster_col_name],
+                            var_name='functional_marker')
+        long_df['metric'] = result_name
+        long_df = long_df.rename(columns={cluster_col_name: 'cell_type', subset_col: 'subset'})
+
+        # combine the two dataframes
+        long_df_all = pd.concat([long_df_all, long_df], axis=0, ignore_index=True)
+
+    return long_df_all
+
+
+
--- python_files/utils_test.py
+++ python_files/utils_test.py
@@ -225,7 +225,7 @@ def test_create_long_df_by_cluster_freq():
     pd.testing.assert_frame_equal(result_subset_region2, expected_region2)
 
 
-def test_create_long_df_by_functional():
+def test_functional_df_helper():
     """Test create_long_df_by_functional"""
     # Create test data
     func_table = pd.DataFrame(
@@ -240,10 +240,10 @@ def test_create_long_df_by_functional():
         }
     )
 
-    # Test with transform_func = np.mean
-    result = utils.create_long_df_by_functional(func_table=func_table, cluster_col_name="cluster",
-                                                drop_cols=['extra_col'],transform_func=np.mean,
-                                                result_name="func_freq")
+    # Test with normalization
+    result = utils.functional_df_helper(func_table=func_table, cluster_col_name="cluster",
+                                        drop_cols=['extra_col'],result_name="func_freq",
+                                        normalize=True)
 
     expected = pd.DataFrame(
         {
@@ -259,13 +259,214 @@ def test_create_long_df_by_functional():
     expected["metric"] = "func_freq"
     pd.testing.assert_frame_equal(result, expected)
 
-    # Test with transform_func = np.sum
-    result = utils.create_long_df_by_functional(func_table=func_table, cluster_col_name="cluster",
-                                                drop_cols=['extra_col'], transform_func=np.sum,
-                                                result_name="func_sum")
+    # Test with no normalization
+    result = utils.functional_df_helper(func_table=func_table, cluster_col_name="cluster",
+                                        drop_cols=['extra_col'], result_name="func_sum",
+                                        normalize=False)
 
     expected.value = [1, 1, 0, 2, 1, 5, 4, 11]
     expected["metric"] = "func_sum"
 
     pd.testing.assert_frame_equal(result, expected)
+
+
+def test_create_long_df_by_functional_counts():
+
+    # create identical cell table as above, but with additional mask column that has only one value
+    func_table = pd.DataFrame(
+        {
+            "fov": ["fov1", "fov1", "fov1", "fov2", "fov2", "fov2"],
+            "cluster": ["cell_type1", "cell_type2", "cell_type2", "cell_type1",
+                        "cell_type2", "cell_type2"],
+            "marker2": [True, True, False, False, True, True],
+            "marker3": [1, 2, 3, 4, 5, 6],
+            "extra_col": [1, 2, 4, 6, 9, 13],
+            "mask": ["region1", "region1", "region1", "region1", "region1", "region1"]
+
+        }
+    )
+
+    # Test without supplying a subset column
+    result_no_subset = utils.create_long_df_by_functional(func_table=func_table, cluster_col_name="cluster",
+                                        drop_cols=['extra_col', 'mask'], result_name="func_count",
+                                                subset_col=None, normalize=False)
+
+    expected = pd.DataFrame(
+        {
+            "fov": ["fov1", "fov1", "fov2", "fov2", "fov1", "fov1", "fov2", "fov2"],
+            "cell_type": ["cell_type1", "cell_type2", "cell_type1", "cell_type2",
+                          "cell_type1", "cell_type2", "cell_type1", "cell_type2"],
+            "functional_marker": ["marker2", "marker2", "marker2", "marker2",
+                                  "marker3", "marker3", "marker3", "marker3"],
+            # boolean markers + numeric markers
+            "value": [1, 1, 0, 2] + [1, 5, 4, 11]
+        }
+    )
+    expected["metric"] = "func_count"
+    expected["subset"] = "all"
+    pd.testing.assert_frame_equal(result_no_subset, expected)
+
+    # Test with supplying a subset column
+    result_subset = utils.create_long_df_by_functional(func_table=func_table, cluster_col_name="cluster",
+                                        drop_cols=['extra_col'], result_name="func_count",
+                                                subset_col="mask", normalize=False)
+
+    result_subset_all = result_subset[result_subset.subset == 'all']
+    result_subset_all.reset_index(drop=True, inplace=True)
+
+    result_subset_region1 = result_subset[result_subset.subset == 'region1']
+    result_subset_region1.reset_index(drop=True, inplace=True)
+
+    # all should be identical to the output when not subsetting
+    pd.testing.assert_frame_equal(result_subset_all, result_no_subset)
+
+    # region1 should be identical to the output when not subsetting, except for the subset column
+    expected_subset_region1 = expected.copy()
+    expected_subset_region1.subset = "region1"
+
+    pd.testing.assert_frame_equal(result_subset_region1, expected_subset_region1)
+
+    # create additional rows with two cells in region2
+    func_table_region2 = pd.DataFrame(
+        {
+            "fov": ["fov1", "fov1"],
+            "cluster": ["cell_type1", "cell_type1"],
+            "marker2": [True, True],
+            "marker3": [1, 2],
+            "extra_col": [1, 2],
+            "mask": ["region2", "region2"]
+        }
+    )
+    func_table = pd.concat([func_table, func_table_region2], axis=0)
+
+    # Test with supplying a subset column
+    result_subset = utils.create_long_df_by_functional(func_table=func_table, cluster_col_name="cluster",
+                                        drop_cols=['extra_col'], result_name="func_count",
+                                                subset_col="mask", normalize=False)
+
+    result_subset_region1 = result_subset[result_subset.subset == 'region1']
+    result_subset_region1.reset_index(drop=True, inplace=True)
+
+    result_subset_region2 = result_subset[result_subset.subset == 'region2']
+    result_subset_region2.reset_index(drop=True, inplace=True)
+
+    # region1 should be identical to the output when not subsetting, except for the subset column
+    pd.testing.assert_frame_equal(result_subset_region1, expected_subset_region1)
+
+    # both cells are positive for marker2, and the sum of marker intensity is 3
+    expected_subset_region2 = pd.DataFrame(
+        {
+            "fov": ["fov1", "fov1"],
+            "cell_type": ["cell_type1", "cell_type1"],
+            "functional_marker": ["marker2", "marker3"],
+            # boolean markers + numeric markers
+            "value": [2, 3]
+        }
+    )
+
+    expected_subset_region2["metric"] = "func_count"
+    expected_subset_region2["subset"] = "region2"
+
+    pd.testing.assert_frame_equal(result_subset_region2, expected_subset_region2)
+
+
+# TODO: This code is largely copied from above
+def test_create_long_df_by_functional_freq():
+
+    # create identical cell table as above, but with additional mask column that has only one value
+    func_table = pd.DataFrame(
+        {
+            "fov": ["fov1", "fov1", "fov1", "fov2", "fov2", "fov2"],
+            "cluster": ["cell_type1", "cell_type2", "cell_type2", "cell_type1",
+                        "cell_type2", "cell_type2"],
+            "marker2": [True, True, False, False, True, True],
+            "marker3": [1, 2, 3, 4, 5, 6],
+            "extra_col": [1, 2, 4, 6, 9, 13],
+            "mask": ["region1", "region1", "region1", "region1", "region1", "region1"]
+
+        }
+    )
+
+    # Test without supplying a subset column
+    result_no_subset = utils.create_long_df_by_functional(func_table=func_table, cluster_col_name="cluster",
+                                        drop_cols=['extra_col', 'mask'], result_name="func_freq",
+                                                subset_col=None, normalize=True)
+
+    expected = pd.DataFrame(
+        {
+            "fov": ["fov1", "fov1", "fov2", "fov2", "fov1", "fov1", "fov2", "fov2"],
+            "cell_type": ["cell_type1", "cell_type2", "cell_type1", "cell_type2",
+                          "cell_type1", "cell_type2", "cell_type1", "cell_type2"],
+            "functional_marker": ["marker2", "marker2", "marker2", "marker2",
+                                  "marker3", "marker3", "marker3", "marker3"],
+            # boolean markers + numeric markers
+            "value": [1, 0.5, 0, 1] + [1, 2.5, 4, 5.5],
+        }
+    )
+    expected["metric"] = "func_freq"
+    expected["subset"] = "all"
+    pd.testing.assert_frame_equal(result_no_subset, expected)
+
+    # Test with supplying a subset column
+    result_subset = utils.create_long_df_by_functional(func_table=func_table, cluster_col_name="cluster",
+                                        drop_cols=['extra_col'], result_name="func_freq",
+                                                subset_col="mask", normalize=True)
+
+    result_subset_all = result_subset[result_subset.subset == 'all']
+    result_subset_all.reset_index(drop=True, inplace=True)
+
+    result_subset_region1 = result_subset[result_subset.subset == 'region1']
+    result_subset_region1.reset_index(drop=True, inplace=True)
+
+    # all should be identical to the output when not subsetting
+    pd.testing.assert_frame_equal(result_subset_all, result_no_subset)
+
+    # region1 should be identical to the output when not subsetting, except for the subset column
+    expected_subset_region1 = expected.copy()
+    expected_subset_region1.subset = "region1"
+
+    pd.testing.assert_frame_equal(result_subset_region1, expected_subset_region1)
+
+    # create additional rows with two cells in region2
+    func_table_region2 = pd.DataFrame(
+        {
+            "fov": ["fov1", "fov1"],
+            "cluster": ["cell_type1", "cell_type1"],
+            "marker2": [True, True],
+            "marker3": [1, 2],
+            "extra_col": [1, 2],
+            "mask": ["region2", "region2"]
+        }
+    )
+    func_table = pd.concat([func_table, func_table_region2], axis=0)
+
+    # Test with supplying a subset column
+    result_subset = utils.create_long_df_by_functional(func_table=func_table, cluster_col_name="cluster",
+                                        drop_cols=['extra_col'], result_name="func_freq",
+                                                subset_col="mask", normalize=True)
+
+    result_subset_region1 = result_subset[result_subset.subset == 'region1']
+    result_subset_region1.reset_index(drop=True, inplace=True)
+
+    result_subset_region2 = result_subset[result_subset.subset == 'region2']
+    result_subset_region2.reset_index(drop=True, inplace=True)
+
+    # region1 should be identical to the output when not subsetting, except for the subset column
+    pd.testing.assert_frame_equal(result_subset_region1, expected_subset_region1)
+
+    # both cells are positive for marker2, and the sum of marker intensity is 3
+    expected_subset_region2 = pd.DataFrame(
+        {
+            "fov": ["fov1", "fov1"],
+            "cell_type": ["cell_type1", "cell_type1"],
+            "functional_marker": ["marker2", "marker3"],
+            # boolean markers + numeric markers
+            "value": [1, 1.5]
+        }
+    )
+
+    expected_subset_region2["metric"] = "func_freq"
+    expected_subset_region2["subset"] = "region2"
+
+    pd.testing.assert_frame_equal(result_subset_region2, expected_subset_region2)
 
