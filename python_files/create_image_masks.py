079c9cabbf436d868cbf07002d9ad6658972acd8
ngreenwald <noahfgreenwald@gmail.com>
2023-06-12 19:47:04 -0700

    updated compartment masks

--- python_files/create_image_masks.py
+++ python_files/create_image_masks.py
@@ -19,10 +19,9 @@ channel_dir = '/Volumes/Shared/Noah Greenwald/TONIC_Cohort/image_data/samples/'
 seg_dir = '/Volumes/Shared/Noah Greenwald/TONIC_Cohort/segmentation_data/deepcell_output'
 mask_dir = '/Volumes/Shared/Noah Greenwald/TONIC_Cohort/data/mask_dir/'
 post_processing_dir = '/Volumes/Shared/Noah Greenwald/TONIC_Cohort/data/post_processing/'
-#cell_table_clusters = pd.read_csv(os.path.join())
+cell_table_clusters = pd.read_csv(os.path.join(post_processing_dir, 'cell_table_clusters.csv'))
 
 folders = list_folders(channel_dir)
-folders = missing_fovs
 
 # create directories to hold masks
 intermediate_dir = os.path.join(mask_dir, 'intermediate_masks')
@@ -35,11 +34,8 @@ if not os.path.exists(individual_dir):
 
 # loop over each FOV and generate the appropriate masks
 for folder in folders:
-    try:
-        ecad = io.imread(os.path.join(channel_dir, folder, 'ECAD.tiff'))
-    except:
-        print('No ECAD channel for ' + folder)
-        continue
+
+    ecad = io.imread(os.path.join(channel_dir, folder, 'ECAD.tiff'))
 
     intermediate_folder = os.path.join(intermediate_dir, folder)
     if not os.path.exists(intermediate_folder):
@@ -58,8 +54,44 @@ for folder in folders:
     tls_label_mask = skimage.measure.label(tls_mask)
     tls_label_mask = morphology.remove_small_objects(tls_label_mask, min_size=25000)
     tls_label_mask = morphology.remove_small_holes(tls_label_mask, area_threshold=7000)
+    tls_label_mask = skimage.measure.label(tls_label_mask)
+
+    # get location of all T cells in image
+    cell_subset = cell_table_clusters[cell_table_clusters['fov'] == folder]
+    cell_subset = cell_subset[cell_subset['cell_cluster_broad'] == 'T']
+    cell_labels = cell_subset['label'].values
+    t_mask = np.isin(seg_label, cell_labels)
+
+    # get location of all B cells in image
+    cell_subset = cell_table_clusters[cell_table_clusters['fov'] == folder]
+    cell_subset = cell_subset[cell_subset['cell_cluster_broad'] == 'B']
+    cell_labels = cell_subset['label'].values
+    b_mask = np.isin(seg_label, cell_labels)
+
+    # create mask to hold T only aggregates
+    tagg_label_mask = np.zeros(tls_label_mask.shape)
+
+    # figure out which TLS objects contain both B and T cells
+    for i in range(1, tls_label_mask.max() + 1):
+        tls_object = tls_label_mask == i
+
+        # get proportion of pixels that are B cells
+        b_pixels = np.sum(b_mask[tls_object])
+        t_pixels = np.sum(t_mask[tls_object])
+        b_prop = b_pixels / (b_pixels + t_pixels)
+
+        if b_prop < 0.2:
+            # not a tls, remove from tls mask and add to tagg
+            tls_label_mask[tls_object] = 0
+            tagg_label_mask[tls_object] = 1
+
+    # convert to binary
+    tls_label_mask = tls_label_mask > 0
     io.imsave(os.path.join(intermediate_folder, 'tls_mask.png'), tls_label_mask.astype(np.uint8),
               check_contrast=False)
+
+    io.imsave(os.path.join(intermediate_folder, 'tagg_mask.png'), tagg_label_mask.astype(np.uint8),
+                check_contrast=False)
 
     # create mask for slide background
     gold = io.imread(os.path.join(channel_dir, folder, 'Au.tiff'))
@@ -67,13 +99,15 @@ for folder in folders:
     gold_mask = utils.create_channel_mask(img=gold, sigma=2, intensity_thresh=350,
                                           min_mask_size=5000, max_hole_size=1000)
 
-    for _ in range(5):
+    # erode edges of gold mask so that it doesn't encroach on other masks
+    for _ in range(15):
         gold_mask = morphology.binary_erosion(gold_mask)
 
+    # any cell can't be in the gold mask
+    gold_mask[seg_label > 0] = 0
     io.imsave(os.path.join(intermediate_folder, 'gold_mask.png'), gold_mask.astype(np.uint8),
                 check_contrast=False)
 
-folders = list_folders(intermediate_dir)
 
 # remove any overlapping pixels from different masks, then save individually
 for folder in folders:
@@ -81,10 +115,12 @@ for folder in folders:
     intermediate_folder = os.path.join(intermediate_dir, folder)
     cancer_mask = io.imread(os.path.join(intermediate_folder, 'cancer_mask.png'))
     tls_mask = io.imread(os.path.join(intermediate_folder, 'tls_mask.png'))
+    tagg_mask = io.imread(os.path.join(intermediate_folder, 'tagg_mask.png'))
     gold_mask = io.imread(os.path.join(intermediate_folder, 'gold_mask.png'))
 
     # create a single unified mask; TLS and background override tumor compartments
     cancer_mask[tls_mask == 1] = 5
+    cancer_mask[tagg_mask == 1] = 6
     cancer_mask[gold_mask == 1] = 0
 
     # save individual masks
@@ -92,8 +128,8 @@ for folder in folders:
     if not os.path.exists(processed_folder):
         os.mkdir(processed_folder)
 
-    for idx, name in zip(range(0, 6), ['empty_slide', 'stroma_core', 'stroma_border',
-                                       'cancer_border', 'cancer_core', 'tls']):
+    for idx, name in zip(range(0, 7), ['empty_slide', 'stroma_core', 'stroma_border',
+                                       'cancer_border', 'cancer_core', 'tls', 'tagg']):
         channel_img = cancer_mask == idx
         io.imsave(os.path.join(processed_folder, name + '.tiff'), channel_img.astype(np.uint8),
                   check_contrast=False)
@@ -103,16 +139,18 @@ area_df = utils.calculate_mask_areas(mask_dir=individual_dir, fovs=folders)
 area_df.to_csv(os.path.join(post_processing_dir, 'fov_annotation_mask_area.csv'), index=False)
 
 # create combined images for visualization
-for folder in folders[:50]:
+for folder in folders[:20]:
     cluster_overlay = io.imread(os.path.join('/Volumes/Shared/Noah Greenwald/TONIC_Cohort/overlay_dir/cell_cluster_overlay', folder + '.png'))
     compartment_overlay = io.imread(os.path.join('/Volumes/Shared/Noah Greenwald/TONIC_Cohort/overlay_dir/compartment_overlay', folder + '.png'))
     gold_chan = io.imread(os.path.join(channel_dir, folder, 'Au.tiff'))
     border_mask = io.imread(os.path.join(intermediate_dir, folder, 'cancer_mask.png'))
     tls_mask = io.imread(os.path.join(intermediate_dir, folder, 'tls_mask.png'))
+    tagg_mask = io.imread(os.path.join(intermediate_dir, folder, 'tagg_mask.png'))
     gold_mask = io.imread(os.path.join(intermediate_dir, folder, 'gold_mask.png'))
 
     # create a single unified mask; TLS and background override tumor compartments
     border_mask[tls_mask == 1] = 5
+    border_mask[tagg_mask == 1] = 6
     border_mask[gold_mask == 1] = 0
 
     # make top row shorter than bottom row
@@ -130,22 +168,7 @@ for folder in folders[:50]:
 
 
 # assign cells to the correct compartment
-assignment_table = utils.assign_cells_to_mask(seg_dir=seg_dir, mask_dir=individual_dir, fovs=folders)
-assignment_table.to_csv(os.path.join(post_processing_dir, 'cell_annotation_mask.csv'), index=False)
-
-#assignment_table = pd.read_csv(os.path.join('/Users/noahgreenwald/Documents/Grad_School/Lab/TNBC/Data/assignment_table.csv'))
-
-#
-# cell_table_short = cell_table_short.loc[cell_table_short['fov'].isin(assignment_table.fov.unique()), :]
-# cell_table_short = cell_table_short.merge(assignment_table, on=['fov', 'label'], how='left')
-# cell_table_short = cell_table_short.rename(columns={'mask_name': 'tumor_region'})
-# cell_table_short.to_csv(os.path.join('/Users/noahgreenwald/Documents/Grad_School/Lab/TNBC/Data', 'combined_cell_table_normalized_cell_labels_updated_clusters_only_kmeans_nh_mask.csv'), index=False)
-#
-# cell_table_func = pd.read_csv(os.path.join('/Users/noahgreenwald/Documents/Grad_School/Lab/TNBC/Data/', 'combined_cell_table_normalized_cell_labels_updated_functional_only.csv'))
-# cell_table_func = cell_table_func.loc[cell_table_func['fov'].isin(assignment_table.fov.unique()), :]
-# cell_table_func = cell_table_func.merge(assignment_table, on=['fov', 'label'], how='left')
-# cell_table_func = cell_table_func.rename(columns={'mask_name': 'tumor_region'})
-# cell_table_func.to_csv(os.path.join('/Users/noahgreenwald/Documents/Grad_School/Lab/TNBC/Data', 'combined_cell_table_normalized_cell_labels_updated_functional_only_mask.csv'), index=False)
-#
-
+for i in range(0, 1400, 200):
+    assignment_table = utils.assign_cells_to_mask(seg_dir=seg_dir, mask_dir=individual_dir, fovs=folders[i:i+200])
+    assignment_table.to_csv(os.path.join(post_processing_dir, 'cell_annotation_mask_{}'.format(i)), index=False)
 
--- python_files/image_overlays.py
+++ python_files/image_overlays.py
@@ -11,44 +11,8 @@ from matplotlib import cm
 from matplotlib import colors
 
 from alpineer import io_utils
+from ark.utils import data_utils
 from skimage.segmentation import find_boundaries
-
-
-# get FOVs of size 2048 x 2048
-image_dir = '/Volumes/Shared/Noah Greenwald/TONIC_Cohort/image_data/samples/'
-plot_dir = '/Users/noahgreenwald/Documents/Grad_School/Lab/TNBC/figures/overlays/'
-
-folders = io_utils.list_folders(image_dir)
-
-keep_folders = []
-
-for folder in folders:
-    test_img = io.imread(os.path.join(image_dir, folder, 'CD3.tiff'))
-    if test_img.shape[0] == 2048:
-        keep_folders.append(folder)
-np.random.shuffle(keep_folders)
-
-folder_df = pd.DataFrame(keep_folders, columns=['folder_name'])
-folder_df.to_csv(os.path.join(plot_dir, 'composite_folder_names.csv'))
-
-
-output_img = np.zeros((5120, 10240))
-
-# final image size: 20 x 40 images
-image_num = 0
-channel_name = 'ECAD.tiff'
-
-for col_num in range(40):
-    for row_num in range(20):
-        img = io.imread(os.path.join(image_dir, keep_folders[image_num], channel_name))
-        img_small = block_reduce(img, block_size=(8, 8), func=np.mean)
-        output_img[row_num * 256:(row_num + 1) * 256, col_num * 256:(col_num + 1) * 256] = img_small
-        image_num += 1
-
-output_img = output_img / np.max(output_img)
-io.imsave(os.path.join(plot_dir, channel_name), output_img)
-
-
 
 def create_cell_overlay(cell_table, seg_folder, fovs, cluster_col, plot_dir, save_names):
     cell_subset = cell_table.copy()
@@ -97,6 +61,44 @@ def create_cell_overlay(cell_table, seg_folder, fovs, cluster_col, plot_dir, sav
         #io.imsave(os.path.join(plot_dir, save_names[idx]), output)
 
 
+# get FOVs of size 2048 x 2048
+image_dir = '/Volumes/Shared/Noah Greenwald/TONIC_Cohort/image_data/samples/'
+plot_dir = '/Users/noahgreenwald/Documents/Grad_School/Lab/TNBC/figures/overlays/'
+
+folders = io_utils.list_folders(image_dir)
+
+keep_folders = []
+
+for folder in folders:
+    test_img = io.imread(os.path.join(image_dir, folder, 'CD3.tiff'))
+    if test_img.shape[0] == 2048:
+        keep_folders.append(folder)
+np.random.shuffle(keep_folders)
+
+folder_df = pd.DataFrame(keep_folders, columns=['folder_name'])
+folder_df.to_csv(os.path.join(plot_dir, 'composite_folder_names.csv'))
+
+
+output_img = np.zeros((5120, 10240))
+
+# final image size: 20 x 40 images
+image_num = 0
+channel_name = 'ECAD.tiff'
+
+for col_num in range(40):
+    for row_num in range(20):
+        img = io.imread(os.path.join(image_dir, keep_folders[image_num], channel_name))
+        img_small = block_reduce(img, block_size=(8, 8), func=np.mean)
+        output_img[row_num * 256:(row_num + 1) * 256, col_num * 256:(col_num + 1) * 256] = img_small
+        image_num += 1
+
+output_img = output_img / np.max(output_img)
+io.imsave(os.path.join(plot_dir, channel_name), output_img)
+
+
+
+
+
 #folders = cell_table_clusters.fov.unique()
 #np.random.shuffle(folders)
 
@@ -112,6 +114,11 @@ fovs = cell_table_clusters.fov.unique()
 create_cell_overlay(cell_table=plot_cell_crops, seg_folder='/Volumes/Shared/Noah Greenwald/TONIC_Cohort/segmentation_data/deepcell_output',
                     fovs=fov_subset, cluster_col='ecm_cluster', plot_dir='/Volumes/Shared/Noah Greenwald/TONIC_Cohort/overlay_dir/ecm_overlay',
                     save_names=['{}.png'.format(x) for x in fov_subset])
+
+# create overlays based on compartment
+create_cell_overlay(cell_table=assignment_table, seg_folder='/Volumes/Shared/Noah Greenwald/TONIC_Cohort/segmentation_data/deepcell_output',
+                    fovs=folders[:20], cluster_col='mask_name', plot_dir='/Volumes/Shared/Noah Greenwald/TONIC_Cohort/overlay_dir/compartment_overlay',
+                    save_names=['{}.png'.format(x) for x in folders[:20]])
 
 
 
@@ -171,6 +178,8 @@ for fov in fov_subset:
     ax[0, 1].imshow(ecm_overlay)
     ax[0, 1].set_title('ECM')
     ax[0, 1].axis('off')
+
+
     ax[0, 2].imshow(VIM_img)
     ax[0, 2].set_title('VIM')
     ax[0, 2].axis('off')
@@ -189,7 +198,7 @@ for fov in fov_subset:
 
 
 
-    plt.savefig(os.path.join('/Volumes/Shared/Noah Greenwald/TONIC_Cohort/overlay_dir/combined_ecm_overlay', fov + '.png'), dpi=300)
-    plt.close()
+plt.savefig(os.path.join('/Volumes/Shared/Noah Greenwald/TONIC_Cohort/overlay_dir/combined_ecm_overlay', fov + '.png'), dpi=300)
+plt.close()
 
 
