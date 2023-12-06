9304296e4a9a7733e8598764b1f4e3a467220d74
Noah F. Greenwald <noahfgreenwald@gmail.com>
2023-12-05 16:25:05 -0800

    template for supplementary plots (#14)

---
+++ python_files/generate_supplementary_plots.py
@@ -0,0 +1,38 @@
+# File with code for generating supplementary plots
+import os
+import shutil
+
+import numpy as np
+import pandas as pd
+import matplotlib
+
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# Panel validation
+
+
+# ROI selection
+
+
+# QC
+
+
+# Image processing
+
+
+# Cell identification and classification
+
+
+
+# Functional marker thresholding
+
+
+
+# Feature extraction
+
+
