657d64814c2803a88624e8c7be5208398c0e6093
alex-l-kong <alkong@ucdavis.edu>
2023-12-15 13:16:39 -0800

    Initial commit of functional marker thresholding plotting

--- python_files/generate_supplementary_plots.py
+++ python_files/generate_supplementary_plots.py
@@ -1,7 +1,7 @@
 # File with code for generating supplementary plots
 import os
 import shutil
-from typing import List, Optional
+from typing import List, Optional, Tuple, Union
 
 import numpy as np
 import pandas as pd
@@ -13,6 +13,8 @@ matplotlib.rcParams['ps.fonttype'] = 42
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from alpineer.io_utils import validate_paths
+from alpineer.misc_utils import verify_in_list
 
 # Panel validation
 
@@ -32,11 +34,80 @@ import seaborn as sns
 
 # Functional marker thresholding
 def functional_marker_thresholding(
-    cell_table: pd.DataFrame, populations, List[str], marker: str,
-    pop_col: str = "cell_meta_cluster", threshold: Optional[float] = None,
+    cell_table: pd.DataFrame, save_dir: Union[str, pathlib.Path], marker: str,
+    populations: List[str], threshold: float, pop_col: str = "cell_meta_cluster",
     percentile: float = 0.999):
-    pass
+    """For a particular marker, visualize its distribution across the entire cohort, plus just 
+    against the specified populations.
 
+    Args:
+        cell_table (pd.DataFrame):
+            Cell table with clustered cell populations
+        marker (str):
+            The marker to visualize the distributions for
+        save_dir (Union[str, pathlib.Path]):
+            The directory to save the marker distribution histograms
+        populations (List[str]):
+            Additional populations to subset on for more distribution plots
+        threshold (float):
+            Value to plot a horizontal line for visualization, determined from Mantis
+        pop_col (str):
+            Column containing the names of the cell populations
+        percentile (float):
+            Cap used to control x axis limits of the plot
+    """
+    # verify save_dir is valid
+    validate_paths([save_dir])
+
+    # Make populations a list if it is a string
+    populations: List[str] = misc_utils.make_iterable(populations, ignore_str=True)
+
+    # Verify that the marker and all populations specified are valid
+    verify_in_list(
+        specified_marker=marker,
+        cell_table_columns=cell_table.columns.values
+    )
+
+    all_populations: np.ndarray = cell_table[pop_col].unique()
+    verify_in_list(
+        specified_populations=populations,
+        cell_table_populations=all_populations
+    )
+
+    # define the subplot grid
+    fig, axs = plt.subplots(1 + len(populations), 1, figsize=[6.4, 2.2 * pop_num], squeeze=False)
+
+    # the first subplot should always be the distribution of the marker against all populations
+    axs[0].hist(
+        cell_table[marker].values,
+        50,
+        density=True,
+        facecolor='g',
+        alpha=0.75,
+        range=(0, np.quantile(cell_table[marker].values))
+    )
+    axs[0].axvline(x=threshold)
+
+    # add additional subplots to the figure based on the specified populations
+    for ax, pop in zip(axs[1:].flat, populations):
+        cell_table_marker_sub: pd.DataFrame = cell_table.loc[
+            cell_table[pop_col] == pop, marker
+        ].values
+        ax.hist(
+            cell_table_marker_sub,
+            50,
+            density=True,
+            facecolor='g',
+            alpha=0.75,
+            range=(0, np.quantile(cell_table_marker_sub))
+        )
+        ax.axvline(x=threshold)
+
+    # save the figure to save_dir
+    fig.savefig(
+        pathlib.Path(save_dir) / f"{marker}_thresholds_{'_'.join(populations)}.png",
+        dpi=300
+    )
 
 # Feature extraction
 
