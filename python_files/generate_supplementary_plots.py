bd75d2eea17a3d88144b16e2a825b58c2ed79602
csowers <camisowers@g.ucla.edu>
2024-01-04 13:25:20 -0800

    cell mask plots

--- python_files/generate_supplementary_plots.py
+++ python_files/generate_supplementary_plots.py
@@ -1,16 +1,15 @@
 # File with code for generating supplementary plots
 import os
-import shutil
+import random
 
 import numpy as np
 import pandas as pd
 import matplotlib
+import seaborn as sns
+from ark.utils.plot_utils import cohort_cluster_plot
 
 matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['ps.fonttype'] = 42
-
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 
 # Panel validation
@@ -20,7 +19,6 @@ import seaborn as sns
 
 
 # QC
-
 
 # Image processing
 
@@ -38,20 +36,20 @@ save_dir = '/Volumes/Shared/Noah Greenwald/TONIC_Cohort/supplementary_figs'
 
 ## cell cluster counts
 sns.histplot(data=cell_table, x="cell_cluster")
-plt.title("Cell Cluster Counts")
-plt.xlabel("Cell Cluster")
-plt.xticks(rotation=75)
-plt.tight_layout()
-plt.savefig(os.path.join(save_dir, "cells_per_cluster.png"), dpi=300)
+matplotlib.pyplot.title("Cell Cluster Counts")
+matplotlib.pyplot.xlabel("Cell Cluster")
+matplotlib.pyplot.xticks(rotation=75)
+matplotlib.pyplot.tight_layout()
+matplotlib.pyplot.savefig(os.path.join(save_dir, "cells_per_cluster.png"), dpi=300)
 
 ## fov cell counts
 cluster_counts = np.unique(cell_table.fov, return_counts=True)[1]
-plt.figure(figsize=(8, 6))
+matplotlib.pyplot.figure(figsize=(8, 6))
 g = sns.histplot(data=cluster_counts, kde=True)
-plt.title("Histogram of Cell Counts per Image")
-plt.xlabel("Number of Cells in an Image")
-plt.tight_layout()
-plt.savefig(os.path.join(save_dir, "cells_per_fov.png"), dpi=300)
+matplotlib.pyplot.title("Histogram of Cell Counts per Image")
+matplotlib.pyplot.xlabel("Number of Cells in an Image")
+matplotlib.pyplot.tight_layout()
+matplotlib.pyplot.savefig(os.path.join(save_dir, "cells_per_fov.png"), dpi=300)
 
 ## cell type composition by tissue location of met
 meta_data = pd.read_csv('/Volumes/Shared/Noah Greenwald/TONIC_Cohort/analysis_files/harmonized_metadata.csv')
@@ -82,15 +80,36 @@ prop_data = prop_data[color_map['cell_cluster_broad']]
 sns.set(rc={'figure.figsize':(14,10)})
 colors = color_map['color']
 prop_data.plot(kind='bar', stacked=True, color=colors)
-plt.ticklabel_format(style='plain', useOffset=False, axis='y')
-plt.gca().set_ylabel("Cell Proportions")
-plt.gca().set_xlabel("Tissue Location")
-plt.xticks(rotation=30)
-plt.title("Cell Type Composition by Tissue Location")
-plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-plt.tight_layout()
-plt.savefig(os.path.join(save_dir, "cell_props_by_tissue_loc.png"), dpi=300)
+matplotlib.pyplot.ticklabel_format(style='plain', useOffset=False, axis='y')
+matplotlib.pyplot.gca().set_ylabel("Cell Proportions")
+matplotlib.pyplot.gca().set_xlabel("Tissue Location")
+matplotlib.pyplot.xticks(rotation=30)
+matplotlib.pyplot.title("Cell Type Composition by Tissue Location")
+matplotlib.pyplot.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+matplotlib.pyplot.tight_layout()
+matplotlib.pyplot.savefig(os.path.join(save_dir, "cell_props_by_tissue_loc.png"), dpi=300)
 
+## colored cell cluster masks from random subset of 20 FOVs
+random.seed(13)
+seg_dir = '/Volumes/Shared/Noah Greenwald/TONIC_Cohort/segmentation_data/deepcell_output'
+
+all_fovs = list(cell_table['fov'].unique())
+fovs = random.sample(all_fovs, 20)
+cell_table_subset = cell_table[cell_table.fov.isin(fovs)]
+
+cohort_cluster_plot(
+    fovs=fovs,
+    seg_dir=seg_dir,
+    save_dir=save_dir,
+    cell_data=cell_table_subset,
+    erode=True,
+    fov_col='fov',
+    label_col='label',
+    cluster_col='cell_cluster_broad',
+    seg_suffix="_whole_cell.tiff",
+    cmap=color_map,
+    display_fig=False,
+)
 
 # Functional marker thresholding
 
