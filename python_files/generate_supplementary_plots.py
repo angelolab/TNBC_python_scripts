bb5e3a9262593bede385e8fb0e1a0845be8a527f
alex-l-kong <alkong@ucdavis.edu>
2024-03-14 14:15:26 -0700

    Add save_separate True for all the Rosetta stitchings

--- python_files/generate_supplementary_plots.py
+++ python_files/generate_supplementary_plots.py
@@ -223,19 +223,23 @@ post_rosetta_dir = "/Volumes/Shared/Noah Greenwald/TONIC_Acquisition/rosetta"
 # NOTE: images not scaled up programmatically, this happens manually in Photoshop
 supplementary_plot_helpers.stitch_before_after_rosetta(
     pre_rosetta_dir, post_rosetta_dir, acquisition_order_viz_dir_rosetta, run_name,
-    [11, 20, 35], "CD4", post_rosetta_subdir="normalized", padding=0, step=1
+    [11, 20, 35], "CD4", post_rosetta_subdir="normalized", padding=0, step=1,
+    save_separate=True
 )
 supplementary_plot_helpers.stitch_before_after_rosetta(
     pre_rosetta_dir, post_rosetta_dir, acquisition_order_viz_dir_rosetta, run_name,
-    [17, 18, 39], "CD56", post_rosetta_subdir="normalized", padding=0, step=1
+    [17, 18, 39], "CD56", post_rosetta_subdir="normalized", padding=0, step=1,
+    save_separate=True
 )
 supplementary_plot_helpers.stitch_before_after_rosetta(
     pre_rosetta_dir, post_rosetta_dir, acquisition_order_viz_dir_rosetta, run_name,
-    [30, 45], "CD31", post_rosetta_subdir="normalized", padding=0, step=1
+    [30, 45], "CD31", post_rosetta_subdir="normalized", padding=0, step=1,
+    save_separate=True
 )
 supplementary_plot_helpers.stitch_before_after_rosetta(
     pre_rosetta_dir, post_rosetta_dir, acquisition_order_viz_dir_rosetta, run_name,
-    [11, 15, 17, 20, 30, 42, 43], "CD8", post_rosetta_subdir="normalized", padding=0, step=1
+    [11, 15, 17, 20, 30, 42, 43], "CD8", post_rosetta_subdir="normalized", padding=0, step=1,
+    save_separate=True
 )
 
 ## show a run with images stitched in acquisition order pre- and post-normalization
