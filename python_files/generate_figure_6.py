36c336e474455947544c6293b7fd08d3c4b0186c
ngreenwald <noahfgreenwald@gmail.com>
2024-04-24 12:42:56 -0700

    new figures

--- python_files/generate_figure_6.py
+++ python_files/generate_figure_6.py
@@ -8,6 +8,7 @@ matplotlib.rcParams['ps.fonttype'] = 42
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 
 base_dir = '/Volumes/Shared/Noah Greenwald/TONIC_Cohort'
@@ -149,10 +150,8 @@ for timepoint in ['baseline', 'on_nivo', 'post_induction']:
             ranked_features_univ_sub_genomic = ranked_features_univ_sub_genomic.loc[ranked_features_univ_sub_genomic.comparison == timepoint, :]
 
             # merge
-            combined_rankings = ranked_features_univ_sub_genomic[['feature_name_unique', 'combined_rank']].merge(ranked_features, on='feature_name_unique')
+            combined_rankings = ranked_features_univ_sub_genomic[['feature_name_unique', 'feature_rank_global', 'importance_score']].merge(ranked_features, on='feature_name_unique')
             combined_rankings = combined_rankings.sort_values('rank_norm')
-            combined_rankings['importance_score'] = combined_rankings['combined_rank']
-            combined_rankings = combined_rankings.rename(columns={'combined_rank': 'feature_rank_global'})
 
         # save csv
         combined_rankings['top_ranked'] = np.logical_and(combined_rankings.coef_norm.values >= 0.3,
@@ -196,6 +195,26 @@ for timepoint in ['baseline', 'on_nivo', 'post_induction']:
         combined_rankings = combined_rankings[['timepoint', 'modality', 'feature_name_unique', 'feature_rank_global', 'rank', 'rank_norm', 'importance_score', 'count', 'coef_norm', 'top_ranked']]
         all_model_rankings = pd.concat([all_model_rankings, combined_rankings])
 
+# plot top features
+sns.stripplot(data=all_model_rankings.loc[all_model_rankings.top_ranked, :], x='timepoint', y='importance_score', hue='modality')
+plt.title('Top ranked features')
+plt.ylim([0, 1.05])
+plt.savefig(os.path.join(plot_dir, 'Figure6_top_ranked_features.pdf'))
+plt.close()
+
+# plot number of times features are selected
+sns.histplot(data=all_model_rankings.loc[all_model_rankings.top_ranked, :], x='count', color='grey', multiple='stack',
+             binrange=(1, 10), discrete=True)
+plt.title('Number of times features are selected')
+plt.savefig(os.path.join(plot_dir, 'Figure6_top_ranked_feature_counts.pdf'))
+plt.close()
+
+sns.histplot(data=all_model_rankings, x='count', color='grey', multiple='stack',
+             binrange=(1, 10), discrete=True)
+plt.title('Number of times features are selected')
+plt.savefig(os.path.join(plot_dir, 'Figure6_all_feature_counts.pdf'))
+plt.close()
+
 # plot venn diagram
 from matplotlib_venn import venn3
 
@@ -217,4 +236,52 @@ mibi_induction = mibi_rankings_top.loc[mibi_rankings_top.timepoint == 'post_indu
 venn3([set(mibi_baseline), set(mibi_nivo), set(mibi_induction)], ('Baseline', 'Nivo', 'Induction'))
 plt.title('MIBI top ranked features')
 plt.savefig(os.path.join(plot_dir, 'Figure6_MIBI_top_ranked_venn.pdf'))
+plt.close()
+
+# compare correlations between top ranked features
+nivo_features_model = all_model_rankings.loc[np.logical_and(all_model_rankings.timepoint == 'on_nivo', all_model_rankings.top_ranked), :]
+nivo_features_model = nivo_features_model.loc[nivo_features_model.modality == 'MIBI', 'feature_name_unique'].values
+
+nivo_features_univariate = ranked_features_univariate.loc[np.logical_and(ranked_features_univariate.comparison == 'on_nivo',
+                                                                         ranked_features_univariate.feature_rank_global <= 100), :]
+
+timepoint_features = pd.read_csv(os.path.join(base_dir, 'analysis_files/timepoint_combined_features.csv'))
+timepoint_features = timepoint_features.loc[timepoint_features.Timepoint == 'on_nivo', :]
+
+timepoint_features_model = timepoint_features.loc[timepoint_features.feature_name_unique.isin(nivo_features_model), :]
+timepoint_features_model = timepoint_features_model[['feature_name_unique', 'normalized_mean', 'Patient_ID']]
+timepoint_features_model = timepoint_features_model.pivot(index='Patient_ID', columns='feature_name_unique', values='normalized_mean')
+
+# get values
+model_corr = timepoint_features_model.corr()
+model_corr = model_corr.where(np.triu(np.ones(model_corr.shape), k=1).astype(np.bool)).values.flatten()
+model_corr = model_corr[~np.isnan(model_corr)]
+
+# get univariate features
+timepoint_features_univariate = timepoint_features.loc[timepoint_features.feature_name_unique.isin(nivo_features_univariate.feature_name_unique), :]
+timepoint_features_univariate = timepoint_features_univariate[['feature_name_unique', 'normalized_mean', 'Patient_ID']]
+timepoint_features_univariate = timepoint_features_univariate.pivot(index='Patient_ID', columns='feature_name_unique', values='normalized_mean')
+
+# get values
+univariate_corr = timepoint_features_univariate.corr()
+univariate_corr = univariate_corr.where(np.triu(np.ones(univariate_corr.shape), k=1).astype(np.bool)).values.flatten()
+univariate_corr = univariate_corr[~np.isnan(univariate_corr)]
+
+corr_values = pd.DataFrame({'correlation': np.concatenate([model_corr, univariate_corr]),
+                            'model': ['model'] * len(model_corr) + ['univariate'] * len(univariate_corr)})
+
+# plot correlations by model
+fig, ax = plt.subplots(1, 1, figsize=(3, 4))
+# sns.stripplot(data=corr_values, x='model', y='correlation',
+#               color='black', ax=ax)
+sns.boxplot(data=corr_values, x='model', y='correlation',
+            color='grey', ax=ax, showfliers=False)
+
+ax.set_title('Feature correlation')
+# ax.set_ylim([-1, 1])
+#ax.set_xticklabels(['Top ranked', 'Other'])
+
+sns.despine()
+plt.tight_layout()
+plt.savefig(os.path.join(plot_dir, 'Figure6_feature_correlation_by_model.pdf'))
 plt.close()
