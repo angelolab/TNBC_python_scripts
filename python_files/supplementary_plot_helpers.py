9aa9630c49dbf9822e0b0b8d244a589f1b2c0165
ngreenwald <noahfgreenwald@gmail.com>
2024-06-19 07:30:07 -0700

    figure 7

--- python_files/supplementary_plot_helpers.py
+++ python_files/supplementary_plot_helpers.py
@@ -48,6 +48,127 @@ MEAN_PANEL_NORM = {
     "Vim": 0.06671803387741954
 }
 
+MARKER_INFO = {
+    "Ki67": {
+        "populations": ["Cancer", "Mast"],
+        "threshold": 0.002,
+        "x_range": (0, 0.012),
+        "x_ticks": np.array([0, 0.004, 0.008, 0.012]),
+        "x_tick_labels": np.array([0, 0.004, 0.008, 0.012]),
+    },
+    "CD38": {
+        "populations": ["Endothelium", "Cancer_EMT"],
+        "threshold": 0.004,
+        "x_range": (0, 0.02),
+        "x_ticks": np.array([0, 0.005, 0.01, 0.015, 0.02]),
+        "x_tick_labels": np.array([0, 0.005, 0.01, 0.015, 0.02]),
+    },
+    "CD45RB": {
+        "populations": ["CD4T", "Stroma"],
+        "threshold": 0.001,
+        "x_range": (0, 0.015),
+        "x_ticks": np.array([0, 0.005, 0.010, 0.015]),
+        "x_tick_labels": np.array([0, 0.005, 0.010, 0.015])
+    },
+    "CD45RO": {
+        "populations": ["CD4T", "Fibroblast"],
+        "threshold": 0.002,
+        "x_range": (0, 0.02),
+        "x_ticks": np.array([0, 0.005, 0.01, 0.015, 0.02]),
+        "x_tick_labels": np.array([0, 0.005, 0.01, 0.015, 0.02])
+    },
+    "CD57": {
+        "populations": ["CD8T", "B"],
+        "threshold": 0.002,
+        "x_range": (0, 0.006),
+        "x_ticks": np.array([0, 0.002, 0.004, 0.006]),
+        "x_tick_labels": np.array([0, 0.002, 0.004, 0.006])
+    },
+    "CD69": {
+        "populations": ["Treg", "Cancer"],
+        "threshold": 0.002,
+        "x_range": (0, 0.008),
+        "x_ticks": np.array([0, 0.002, 0.004, 0.006, 0.008]),
+        "x_tick_labels": np.array([0, 0.002, 0.004, 0.006, 0.008])
+    },
+    "GLUT1": {
+        "populations": ["Cancer_EMT", "M2_Mac"],
+        "threshold": 0.002,
+        "x_range": (0, 0.02),
+        "x_ticks": np.array([0, 0.005, 0.01, 0.015, 0.02]),
+        "x_tick_labels": np.array([0, 0.005, 0.01, 0.015, 0.02])
+    },
+    "IDO": {
+        "populations": ["APC", "M1_Mac"],
+        "threshold": 0.001,
+        "x_range": (0, 0.003),
+        "x_ticks": np.array([0, 0.001, 0.002, 0.003]),
+        "x_tick_labels": np.array([0, 0.001, 0.002, 0.003])
+    },
+    "PD1": {
+        "populations": ["CD8T", "Stroma"],
+        "threshold": 0.0005,
+        "x_range": (0, 0.002),
+        "x_ticks": np.array([0, 0.0005, 0.001, 0.0015, 0.002]),
+        "x_tick_labels": np.array([0, 0.0005, 0.001, 0.0015, 0.002])
+    },
+    "PDL1": {
+        "populations": ["Cancer", "Stroma"],
+        "threshold": 0.001,
+        "x_range": (0, 0.003),
+        "x_ticks": np.array([0, 0.001, 0.002, 0.003]),
+        "x_tick_labels": np.array([0, 0.001, 0.002, 0.003]),
+    },
+    "HLA1": {
+        "populations": ["APC", "Stroma"],
+        "threshold": 0.001,
+        "x_range": (0, 0.025),
+        "x_ticks": np.array([0, 0.0125, 0.025]),
+        "x_tick_labels": np.array([0, 0.0125, 0.025])
+    },
+    "HLADR": {
+        "populations": ["APC", "Neutrophil"],
+        "threshold": 0.001,
+        "x_range": (0, 0.025),
+        "x_ticks": np.array([0, 0.0125, 0.025]),
+        "x_tick_labels": np.array([0, 0.0125, 0.025])
+    },
+    "TBET": {
+        "populations": ["NK", "B"],
+        "threshold": 0.0015,
+        "x_range": (0, 0.0045),
+        "x_ticks": np.array([0, 0.0015, 0.003, 0.0045]),
+        "x_tick_labels": np.array([0, 0.0015, 0.003, 0.0045])
+    },
+    "TCF1": {
+        "populations": ["CD4T", "M1_Mac"],
+        "threshold": 0.001,
+        "x_range": (0, 0.003),
+        "x_ticks": np.array([0, 0.001, 0.002, 0.003]),
+        "x_tick_labels": np.array([0, 0.001, 0.002, 0.003])
+    },
+    "TIM3": {
+        "populations": ["Monocyte", "Endothelium"],
+        "threshold": 0.001,
+        "x_range": (0, 0.004),
+        "x_ticks": np.array([0, 0.001, 0.002, 0.003, 0.004]),
+        "x_tick_labels": np.array([0, 0.001, 0.002, 0.003, 0.004])
+    },
+    "Vim": {
+        "populations": ["Endothelium", "B"],
+        "threshold": 0.002,
+        "x_range": (0, 0.06),
+        "x_ticks": np.array([0, 0.02, 0.04, 0.06]),
+        "x_tick_labels": np.array([0, 0.02, 0.04, 0.06])
+    },
+    "Fe": {
+        "populations": ["Fibroblast", "Cancer"],
+        "threshold": 0.1,
+        "x_range": (0, 0.3),
+        "x_ticks": np.array([0, 0.1, 0.2, 0.3]),
+        "x_tick_labels": np.array([0, 0.1, 0.2, 0.3]),
+    }
+}
 
 # generate stitching/annotation function, used by panel validation and acquisition order tiling
 def stitch_and_annotate_padded_img(image_data: xr.DataArray, padding: int = 25,
